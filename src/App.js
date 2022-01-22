3b9b1dfac26edd52c19c8765386cb90c372d503f
hasnain-nisan <68366713+hasnain-nisan@users.noreply.github.com>
2022-01-22 12:02:22 +0600

    header component designed

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ import Map from './components/Map/Map'
 
 const App = () => {
   return (
-      <div className="xl:container h-screen w-screen">
+      <div className="h-screen w-screen m-0 p-0 ">
           <div className="flex flex-col h-full w-full justify-center">
-            <div className="h-[10%] bg-blue-200">
+            <div className="h-[10%] bg-indigo-300">
                 <Header />
             </div>
-            <div className="h-[90%] flex divide-x-2 divide-black">
-                <div className="w-2/5 h-full bg-red-100">
+            <div className="h-[90%] flex flex-col md:flex-row divide-x-2 divide-black">
+                <div className="w-full md:w-2/5 h-full bg-red-100">
                     <List/>
                 </div>
-                <div className="w-3/5 h-full bg-indigo-100">
+                <div className="w-full md:w-3/5 h-full bg-indigo-100">
                     <Map/>
                 </div>
             </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import {Autocomplete} from '@react-google-maps/api'
+import {SiYourtraveldottv} from 'react-icons/si'
+
 
 const Header = () => {
-  return <div>
-      Header
-  </div>;
+  return (
+    <div class="fixed flex items-center justify-between w-full h-[10%] p-2 shadow-lg">
+      <div className="flex flex-col sm:flex-row items-center px-3">
+        <SiYourtraveldottv className="text-3xl font-bold text-black mr-1" />
+        <h1 className="tracking-wide text-xl text-black">𝕿𝖗𝖆𝖛𝖊𝖑 𝕮𝖔𝖒𝖕𝖆𝖓𝖎𝖔𝖓</h1>
+      </div>
+      <div className="">
+        <div class="bg-indigo-200 mr-4 w-50 h-10 space-x-4 rounded-lg flex items-center justify-center border border-gray-200 shadow-md">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6 opacity-50 ml-[8px] md:ml-[15px]"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+          <input
+            class="bg-indigo-200 outline-none placeholder-gray-700 w-[145px] md:w-full"
+            type="text"
+            placeholder="𝘌𝘹𝘱𝘭𝘰𝘳𝘦 𝘵𝘩𝘦 𝘸𝘰𝘳𝘭𝘥"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
